04fef118ca6503d7c97e96480077558beef8d35f
AlexxKai <alexurumart@gmail.com>
2024-01-12 21:37:07 +0100

    aiudaaaa

---
+++ prueba.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    dropdownMenu.style.display = (dropdownMenu.style.display === "block") ? "none" : "block";
+  }
+  
