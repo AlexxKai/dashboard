5ef15065982b94ed5770d5ef6f3cef102dcc2ff2
AlexxKai <alexurumart@gmail.com>
2024-01-19 16:56:54 +0100

    new

--- prueba.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-    var dropdownMenu = document.getElementById("dropdownMenu");
-    dropdownMenu.style.display = (dropdownMenu.style.display === "block") ? "none" : "block";
-  }
-  
