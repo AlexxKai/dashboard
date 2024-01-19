9df1d96b8f7cb5feac68f40128e734bda696a871
AlexxKai <alexurumart@gmail.com>
2024-01-19 21:29:06 +0100

    new

--- js/button.js
+++ js/button.js
@@ -1,15 +1,32 @@
-// Función para mostrar u ocultar el menú
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.style.display = (menu.style.display === "block") ? "none" : "block";
-}
+document.addEventListener('DOMContentLoaded', function () {
+  var dropdownBtn = document.getElementById('dropdownBtn');
+  var dropdownContent = document.querySelector('.dropdown-content');
 
-// Cerrar el menú si se hace clic fuera del botón y del menú
-document.addEventListener("click", function (event) {
-    var menu = document.getElementById("menu");
-    var button = document.querySelector("button");
-
-    if (event.target !== button && event.target !== menu && !menu.contains(event.target)) {
-        menu.style.display = "none";
+  // Función para mostrar u ocultar el menú desplegable
+  function toggleDropdown() {
+    if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none';
+    } else {
+      dropdownContent.style.display = 'block';
     }
+  }
+
+  // Evento de clic en el botón
+  dropdownBtn.addEventListener('click', function () {
+    toggleDropdown();
+  });
+
+  // Evento de clic en la imagen dentro del botón
+  dropdownBtn.querySelector('img').addEventListener('click', function (event) {
+    // Evitar que el clic en la imagen propague al contenedor del botón
+    event.stopPropagation();
+    toggleDropdown();
+  });
+
+  // Cerrar el menú desplegable si se hace clic fuera de él
+  window.addEventListener('click', function (event) {
+    if (!event.target.matches('#dropdownBtn') && dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none';
+    }
+  });
 });
